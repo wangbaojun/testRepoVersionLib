e494b72ba3d09fed59e285ff5b989be5d1a53e19
netease <bjwangbaojun@corp.netease.com>
2016-04-21 10:37:58 +0800

    1.0.1

--- O2View/O2View1.swift
+++ O2View/O2View1.swift
@@ -6,17 +6,4 @@
 //  Copyright © 2015年 jd.com. All rights reserved.
 //
 
-import UIKit
 
-public class O2View: UIView {
-    
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor.redColor()
-    }
-
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-}
